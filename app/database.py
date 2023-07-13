990fc2239519ce55576ee417c79d6474f5f8f2b9
Rezza Nafi <zafitract@gmail.com>
2023-07-13 13:09:50 +0700

    feat: db config

--- app/database.py
+++ app/database.py
@@ -3,8 +3,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 import app.models
 
+from app.config import DBConfig
+
 async def init_db():
-    db = AsyncIOMotorClient("mongodb://127.0.0.1:27017")
+    host = DBConfig.HOST
+    name = DBConfig.NAME
+    username = DBConfig.USERNAME
+    password = DBConfig.PASSWORD
+
+    db = AsyncIOMotorClient(f"mongodb://{username}:{password}@{host}/?authSource={name}")
     document_models = [ "app.models." + model for model in app.models.__all__ ]
 
-    await init_beanie(db.aeon_dev, document_models=document_models)
+    await init_beanie(db[name], document_models=document_models)
