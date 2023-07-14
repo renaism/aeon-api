400ed9d137d9445110f865e3512bbba516b2264b
Rezza Nafi <zafitract@gmail.com>
2023-07-14 14:54:39 +0700

    feat: encode db url

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -8,10 +10,11 @@ from app.config import DBConfig
 async def init_db():
     host = DBConfig.HOST
     name = DBConfig.NAME
-    username = DBConfig.USERNAME
-    password = DBConfig.PASSWORD
+    username = urllib.parse.quote(DBConfig.USERNAME, safe="")
+    password = urllib.parse.quote(DBConfig.PASSWORD, safe="")
+    url = f"mongodb://{username}:{password}@{host}/?authSource={name}"
 
-    db = AsyncIOMotorClient(f"mongodb://{username}:{password}@{host}/?authSource={name}")
+    db = AsyncIOMotorClient(url)
     document_models = [ "app.models." + model for model in app.models.__all__ ]
 
     await init_beanie(db[name], document_models=document_models)
